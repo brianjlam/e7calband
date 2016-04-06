84681689a703928307e9d9e343881c8d236315c2
Brian <blamm9@gmail.com>
2016-04-06 01:00:26 -0700

    beta v0.1

--- calband_transition.m
+++ calband_transition.m
@@ -1,31 +1,37 @@
 function [instructions] = calband_transition(initial_formation,target_formation,max_beats)
 % main function
-
-tag_targetform = tagFormation(target_formation);
-nb = sum(sum(initial_formation));
-
+disp(initial_formation)
+tag_targetform = tagFormation(initial_formation, target_formation)
+nb = sum(sum(target_formation));
+instructions = struct('i_target',[],'j_target',[],'wait',[],'direction',[]);
+instructions = repmat(instructions,1,nb);
 for tag = 1:nb
-    [i_initial,j_initial] = find(tag_initialform == tag); % finds initial coords of marcher
+    disp(tag)
+    [i_initial,j_initial] = find(initial_formation == tag); % finds initial coords of marcher
     [i_target,j_target]   = find(tag_targetform == tag); % finds final coords
     dRow = i_target - i_initial;
     dCol = j_target - j_initial;
-    if dRow==0 && d_Col==0 % case when no move is needed
+    if dRow == 0 && dCol == 0 % when no move is needed
         direction = '.';
         wait = max_beats;
     else % case when move needed
-        switch dRow
-            case dRow < 0
-                dirWE = repmat('W',1,abs(dRow));
-            case dRow > 0
-                dirWE = repmat('E',1,abs(dRow));
+        if dRow < 0
+            dirWE = 'W';
+        elseif dRow > 0
+            dirWE = 'E';
+        else
+            dirWE = '';
         end
-        switch dCol
-            case dCol < 0
-                dirNS = repmat('S',1,abs(dRow));
-            case dCol > 0
-                dirNS = repmat('N',1,abs(dRow));
+        
+        if dCol < 0
+            dirSN = 'S';
+        elseif dCol > 0
+            dirSN = 'N';
+        else
+            dirSN = '';
         end
-        direction = strcat('dirWE','dirSN');
+        wait = 0;
+        direction = strcat(dirWE,dirSN)
     end
     instructions(tag).i_target = i_target;
     instructions(tag).j_target = j_target;

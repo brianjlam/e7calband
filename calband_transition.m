44dec8c0cc56352f0354165c5b860d1983c16a8d
Brian <blamm9@gmail.com>
2016-04-24 00:14:24 -0700

    minor changes

--- calband_transition.m
+++ calband_transition.m
@@ -1,12 +1,11 @@
 function [instructions] = calband_transition(initial_formation,target_formation,max_beats)
 % main function
 disp(initial_formation)
-tag_targetform = tagFormation(initial_formation, target_formation)
+tag_targetform = minDistTagger(initial_formation, target_formation);
 nb = sum(sum(target_formation));
 instructions = struct('i_target',[],'j_target',[],'wait',[],'direction',[]);
 instructions = repmat(instructions,1,nb);
 for tag = 1:nb
-    disp(tag)
     [i_initial,j_initial] = find(initial_formation == tag); % finds initial coords of marcher
     [i_target,j_target]   = find(tag_targetform == tag); % finds final coords
     dRow = i_target - i_initial;
@@ -31,7 +30,7 @@ for tag = 1:nb
             dirSN = '';
         end
         wait = 0;
-        direction = strcat(dirWE,dirSN)
+        direction = strcat(dirWE,dirSN);
     end
     instructions(tag).i_target = i_target;
     instructions(tag).j_target = j_target;

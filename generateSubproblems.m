16aff37277f83133e9bec47e0af8c70dba79c6f6
Brian <blamm9@gmail.com>
2016-04-29 14:38:16 -0700

    add comments for Tina Chow to read

--- generateSubproblems.m
+++ generateSubproblems.m
@@ -1,4 +1,6 @@
 function [initial_sub1, initial_sub2, target_sub1, target_sub2] = generateSubproblems(initial_formation, target_formation, split) 
+% creates subformations given a splitpoint and the complete initial and
+% target formations
 initial_sub1 = initial_formation(:,1:split);
 initial_sub2 = initial_formation(:,split+1:end);
 n = sum(sum(initial_sub1~=0)); % counts number of marchers in subproblem
